ff1c66c8e60a31f0fabe495f67f0da7ea3643d70
Lorenzo Bombardini <lorenzo.bombardini@studio.unibo.it>
2021-07-05 17:44:03 +0200

    Create Character, EnemyCharacter and PlayerCharacter

---
+++ OOP-Bombardini/OOP-Bombardini/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Bombardini
+{
+    class Character
+    {
+        internal string Name { get; set; }
+        internal int Hp { get; set; }
+        internal Weapon Weapon { get; set; }
+
+        public Character(String name, int hp, Weapon weapon)
+        {
+            Name = name;
+            Hp = hp;
+            Weapon = weapon;
+        }
+
+
+    }
+
+    class EnemyCharacter : Character
+    {
+        internal Weapon DropWeapon { get; set; }
+        public EnemyCharacter(Character character, Weapon dropWeapon) : base(character.Name, character.Hp, character.Weapon)
+        {
+            DropWeapon = dropWeapon;
+        }
+    }
+
+    class PlayerCharacter : Character
+    {
+        public PlayerCharacter(Character character) : base(character.Name, character.Hp, character.Weapon) { }
+    }
+
+
+}
